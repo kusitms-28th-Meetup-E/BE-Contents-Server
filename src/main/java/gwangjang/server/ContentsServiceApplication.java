1b9c0188a33af5fd859ce6b1682f3af657e481da
seungyeonnnnnni <jeongsy8928@soongsil.ac.kr>
2023-10-23 12:32:24 +0900

    feat : project init

---
+++ src/main/java/gwangjang/server/ContentsServiceApplication.java
@@ -0,0 +1,13 @@
+package gwangjang.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentsServiceApplication.class, args);
+	}
+
+}
