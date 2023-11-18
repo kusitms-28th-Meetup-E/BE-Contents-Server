fd7ff0d9ea3f9ae0f9da5de72668a5e379012c01
eojin0814 <leeeojin6@gmail.com>
2023-11-18 16:59:52 +0900

    feat : feing 통신

--- src/main/java/gwangjang/server/domain/morpheme/presentation/ContentsController.java
+++ src/main/java/gwangjang/server/domain/morpheme/presentation/ContentsController.java
@@ -1,22 +1,34 @@
 package gwangjang.server.domain.morpheme.presentation;
 
+import gwangjang.server.domain.morpheme.application.dto.req.TotalReq;
 import gwangjang.server.domain.morpheme.domain.service.ContentsService;
+import gwangjang.server.global.feign.client.FindKeywordFeignClient;
+import gwangjang.server.global.response.SuccessResponse;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor(onConstructor_ = {@Autowired})
 public class ContentsController {
 
     private final ContentsService contentsService;
+    private final FindKeywordFeignClient findKeywordFeignClient;
 
-
+    @GetMapping("/keyword/test")
+    public ResponseEntity<SuccessResponse<List<TotalReq>>> getKeyword (){
+        return findKeywordFeignClient.getAll();
+    }
     @Scheduled(cron = "0 0 0 * * *")
     public Mono<Void> analysis() {
         String msg = "이태원 참사";
